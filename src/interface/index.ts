7b435db56107514a1a95a7fb9fa5066e618763ab
kidohy15 <kidohy307@gmail.com>
2024-01-04 00:58:36 +0900

    Marker, StoreBox 생성

---
+++ src/interface/index.ts
@@ -0,0 +1,10 @@
+export interface StoreType {
+  tel_no: string; // 가게 전화번호
+  cob_code_nm: string; // 판매 업종 코드
+  bizcnd_code_nm: string; // 커스텀 이미지를 위한 카테고리 코드 네임
+  upso_nm: string; // 가게명, 업소명
+  x_cnts: string; // x축
+  y_dnts: string; // y축
+  rdn_code_nm: string; // 도로명 주소
+  crtfc_gbn_nm: string; // 식품인증 구분명
+}

d54ed0eea596c8f30be66dba9d405c61ab79460f
kidohy15 <kidohy307@gmail.com>
2024-01-06 14:11:19 +0900

    API Routes prisma 데이터 연동

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,10 +1,24 @@
+// 기존 타입 코드
+// export interface StoreType {
+//   tel_no: string; // 가게 전화번호
+//   cob_code_nm: string; // 판매 업종 코드
+//   bizcnd_code_nm: string; // 커스텀 이미지를 위한 카테고리 코드 네임
+//   upso_nm: string; // 가게명, 업소명
+//   x_cnts: string; // x축
+//   y_dnts: string; // y축
+//   rdn_code_nm: string; // 도로명 주소
+//   crtfc_gbn_nm: string; // 식품인증 구분명
+// }
+
+// 수정한 타입 코드
 export interface StoreType {
-  tel_no: string; // 가게 전화번호
-  cob_code_nm: string; // 판매 업종 코드
-  bizcnd_code_nm: string; // 커스텀 이미지를 위한 카테고리 코드 네임
-  upso_nm: string; // 가게명, 업소명
-  x_cnts: string; // x축
-  y_dnts: string; // y축
-  rdn_code_nm: string; // 도로명 주소
-  crtfc_gbn_nm: string; // 식품인증 구분명
+  id: number;
+  phone: string | null; // 가게 전화번호
+  address: string | null; // 도로명 주소
+  lat: string | null; // x축
+  lng: string | null; // y축
+  name: string | null; // 가게명, 업소명
+  category: string | null; // 커스텀 이미지를 위한 카테고리 코드 네임
+  storeType: string | null; // 판매 업종 코드
+  foodCertifyName: string | null; // 식품인증 구분명
 }
--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -1,13 +1,19 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreType } from "@/interface";
+import { PrismaClient } from "@prisma/client";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<StoreType[]>
 ) {
-  const stores = (await import("../../data/store_data.json"))[
-    "DATA"
-  ] as StoreType[];
+  // 데이터를 하나씩 직접 import 한 방식 -> prisma 로 변경함
+  // const stores = (await import("../../data/store_data.json"))[
+  //   "DATA"
+  // ] as StoreType[];
+
+  // prisma 로 데이터를 가져온다
+  const prisma = new PrismaClient();
+  const stores = await prisma.store.findMany();
 
   res.status(200).json(stores);
 }

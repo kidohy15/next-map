2273e9358f4be0f75ba957d43a9421b4ec018cd9
kidohy15 <kidohy307@gmail.com>
2024-01-11 05:53:24 +0900

    Next-auth 세팅

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,22 @@
+import NextAuth from "next-auth";
+// import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+
+  // Configure one or more authentication providers
+  providers: [
+    // ...add more providers here
+    GoogleProvider({
+      clientId: "test",
+      clientSecret: "test",
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);

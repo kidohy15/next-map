29ed56e1bb5a7b278bd31011ca3b2d0895e4a852
kidohy15 <kidohy307@gmail.com>
2024-01-11 19:03:01 +0900

    google 로그인 인증 구현 및 로그아웃 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
+import { signOut, useSession } from "next-auth/react";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const { data, status } = useSession();
+
+  // console.log(useSession());
+  console.log(data, status);
   return (
     <>
       <div className="navbar">
@@ -22,9 +27,15 @@ export default function Navbar() {
           <Link href="/users/likes" className="navbar__list--item">
             찜한 가게
           </Link>
-          <Link href="/api/auth/signin" className="navbar__list--item">
-            로그인
-          </Link>
+          {status === "authenticated" ? (
+            <button type="button" onClick={() => signOut()}>
+              로그아웃
+            </button>
+          ) : (
+            <Link href="/api/auth/signin" className="navbar__list--item">
+              로그인
+            </Link>
+          )}
         </div>
         {/* mobile button */}
         <div
@@ -35,6 +46,7 @@ export default function Navbar() {
           {isOpen ? <AiOutlineClose /> : <BiMenu />}
         </div>
       </div>
+
       {/* mobile navbar */}
       {isOpen && (
         <div className="navbar--mobile">
@@ -48,7 +60,10 @@ export default function Navbar() {
             <Link href="/users/likes" className="navbar__list--item--mobile">
               찜한 가게
             </Link>
-            <Link href="/api/auth/signin" className="navbar__list--item--mobile">
+            <Link
+              href="/api/auth/signin"
+              className="navbar__list--item--mobile"
+            >
               로그인
             </Link>
           </div>

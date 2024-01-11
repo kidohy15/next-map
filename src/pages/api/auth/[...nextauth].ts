40f04c6f94db19f7eff3bac06b1ea1d9f41d251e
kidohy15 <kidohy307@gmail.com>
2024-01-12 01:29:10 +0900

    Naver 로그인 인증 구현 및 커스텀 로그인 페이지 구현

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 import GoogleProvider from "next-auth/providers/google";
+import NaverProvider from "next-auth/providers/naver";
 
 const prisma = new PrismaClient();
 
@@ -16,7 +17,15 @@ export const authOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || "",
+      clientSecret: process.env.NAVER_CLIENT_SECRET || "",
+    }),
   ],
+  pages: {
+    // 외부 API 로그인 인증 페이지를 커스텀 페이지로 이동하게 함
+    signIn: "/users/login",
+  },
 };
 
 export default NextAuth(authOptions);

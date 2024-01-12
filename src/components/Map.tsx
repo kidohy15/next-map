defd6836c9e9d73467366eeed4ae36e6bac5b7a1
kidohy15 <kidohy307@gmail.com>
2024-01-13 05:17:40 +0900

    상세 페이지 마커 구현

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -12,18 +12,26 @@ declare global {
 const DEFAULT_LAT = 37.497625203;
 const DEFAULT_lng = 127.03088379;
 
+const DEFAULT_ZOOM = 3;
+
 interface MapProps {
   setMap: Dispatch<SetStateAction<any>>;
+  lat?: string | null;
+  lng?: string | null;
+  zoom?: number;
 }
 
-export default function Map({ setMap }: MapProps) {
+export default function Map({ setMap, lat, lng, zoom }: MapProps) {
   const loadKakaoMap = () => {
     // kakao map 로드
     window.kakao.maps.load(() => {
       const mapContainer = document.getElementById("map");
       const mapOption = {
-        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_lng),
-        level: 3,
+        center: new window.kakao.maps.LatLng(
+          lat ?? DEFAULT_LAT,
+          lng ?? DEFAULT_lng
+        ),
+        level: zoom ?? DEFAULT_ZOOM,
       };
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
